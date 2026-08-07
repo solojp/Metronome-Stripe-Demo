0c2973118a1240fc12ce46c2b05aba970433e35e
solojp <jpperezrocha98@gmail.com>
2026-08-07 14:25:27 -0600

    Update

--- Metronome/server.js
+++ Metronome/server.js
@@ -13,9 +13,6 @@ app.post('/sign-up', async (req, res) => {
     try { const stripeCustomer = await stripe.customers.create ({
             name: req.body.name,
             email: req.body.email,
-            metadata: {
-                'username': req.body.username
-            }
         });
         const metronomeCustomer = await metronome.v1.customers.create({
             name: stripeCustomer.name,
@@ -30,7 +27,7 @@ app.post('/sign-up', async (req, res) => {
             }]
         });
         res.json({
-            username: stripeCustomer.metadata.username, id: stripeCustomer.id
+            id: stripeCustomer.id
             // To do: Cache username to simulate logged in user. 
         });
     }
@@ -41,13 +38,102 @@ app.post('/sign-up', async (req, res) => {
 })
 
 app.post('/create-checkout-session', async (req, res) => {
-    const session = await stripe.checkout.session.create({
-        mode: "setup",
-        customer: customer.stripeCustomerId,
-        payment_method_types: ["card"],
-        ui_mode: "form",
-        success_url: "https://stripe.com"
-    })
+    try {
+        const session = await stripe.checkout.sessions.create({
+            mode: "setup",
+            customer: 'cus_V1y0au647WniAB',
+            payment_method_types: ["card"],
+            ui_mode: "hosted_page",
+            success_url: "https://stripe.com"
+        });
+        res.status(200).json({ url: session.url });  
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+        console.error("Failed to save payment method: " ,error.message);
+    }
+    
 })
 
+app.post('/create-contract', async (req, res) => {
+    try {
+        const date = new Date();
+        date.setMinutes(0, 0, 0);
+        const startingAt = date.toISOString();
+        const contract = await metronome.v1.contracts.create({
+            customer_id: '532f2699-d8c9-42d7-b8c1-a8922b707683',
+            rate_card_id: '9baa9942-a550-47b0-80ed-637c51c87118',
+            starting_at: startingAt,
+            billing_provider_configuration: {
+                billing_provider: 'stripe',
+                delivery_method: 'direct_to_billing_provider'
+            },
+            subscriptions:[{
+                initial_quantity: 1,
+                subscription_rate: {
+                    billing_frequency: 'monthly',
+                    product_id: 'a627786d-00c3-42df-99d1-d5787791b5e7',
+                },
+                temporary_id: 'monthly_plan',
+                collection_schedule: 'advance',
+                proration: {
+                    is_prorated: true,
+                    invoice_behavior: 'bill_immediately'
+                },
+                billing_cycle_config: {
+                    anchor_date: date,
+                    invoice_placement: 'ON_SCHEDULED_INVOICE'
+                },
+            }],
+            recurring_credits: [{
+                starting_at: startingAt,
+                product_id: '266900ae-f09a-4df9-ac09-fdc3bd2fe2c6',
+                access_amount: {
+                    unit_price: 1000,
+                    credit_type_id: 'f984e650-b20d-4a95-af49-970d4e233cdb',
+                },
+                priority: 1,
+                commit_duration: {
+                    value: 1,
+                    unit:'periods'
+                },
+                recurrence_frequency: 'monthly',
+                subscription_config: {
+                    subscription_id: "monthly_plan",
+                    apply_seat_increase_config: {
+                        is_prorated: true
+                    },
+                },
+            }],
+        });
+        res.status(200).json({ contract });
+        console.log('Contract Created');
+        
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+        console.error('Failed to create contract', error.message);
+    }
+})
+
+app.post('/post-usage', async (req, res) => {
+    try {
+        const date = new Date();
+        const timeStamp = date.toISOString();
+        const usage = await metronome.v1.usage.ingest({ usage: [{
+            transaction_id: crypto.randomUUID(),
+            customer_id: '532f2699-d8c9-42d7-b8c1-a8922b707683',
+            event_type: 'usage',
+            timestamp: timeStamp,
+            properties: {
+                input_tokens: 20,
+                user_id: '001',
+            }
+        }]});
+        res.status(200).json({  });
+        console.log('usage logged');
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+        console.error('Failed to log usage ', error.message);
+    }
+})
+ 
 app.listen(4242, () => console.log('Runing on port 4242'));
